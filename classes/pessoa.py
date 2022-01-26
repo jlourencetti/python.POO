9a766476b86faedaca12a09ff48eaafb39d33995
jlourencetti <97122075+jlourencetti@users.noreply.github.com>
2022-01-25 23:51:26 -0400

    segundo commit

--- classes/pessoa.py
+++ classes/pessoa.py
@@ -1,7 +1,10 @@
-from pickle import FALSE
+
+from datetime import datetime
 
 
 class Pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
     def __init__(self, nome, idade, comendo=False, falando=False):
         self.nome = nome
         self.idade = idade
@@ -21,6 +24,7 @@ class Pessoa:
         print(f'{self.nome} está falando sobre {assunto}')
         self.falando = True
 
+
     def comer(self, alimento):
         if self.comendo:
             print(f'{self.nome} já está comendo.')
@@ -38,9 +42,17 @@ class Pessoa:
         print(f'{self.nome} parou de comer.')
         self.comendo = False
 
+    
+    def get_ano_nascimento(self):
+        print(f'{self.ano_atual - self.idade} ano de nascimento de {self.nome}')
+        return 
 
-pessoa_1 = Pessoa('Juliano', 37)
+
+pessoa_1 = Pessoa('Juliano', 38)
+pessoa_2 = Pessoa('Roberta', 37)
 
 pessoa_1.comer('Maçã')
 pessoa_1.parar_comer()
 pessoa_1.falar('Carro')
+pessoa_1.get_ano_nascimento()
+pessoa_2.get_ano_nascimento()
