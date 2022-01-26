9c9e5faf04dc01d9187f46a32f2f42f2640ebeec
jlourencetti <97122075+jlourencetti@users.noreply.github.com>
2022-01-25 23:30:11 -0400

    primeiro commit

---
+++ classes/main.py
@@ -0,0 +1,7 @@
+from pessoa import Pessoa
+
+pessoa_1 = Pessoa('Juliano', 37)
+
+pessoa_1.comer('Maçã')
+pessoa_1.comer('Maçã')
+
---
+++ classes/pessoa.py
@@ -0,0 +1,46 @@
+from pickle import FALSE
+
+
+class Pessoa:
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} não pode falar comendo')
+            return
+
+        if self.falando:
+            print(f'{self.nome} já está falando.')
+            return
+
+        print(f'{self.nome} está falando sobre {assunto}')
+        self.falando = True
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} já está comendo.')
+            return
+
+        print(f'{self.nome} está comendo {alimento}')
+        self.comendo = True
+
+
+    def parar_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo.')
+            return
+        
+        print(f'{self.nome} parou de comer.')
+        self.comendo = False
+
+
+pessoa_1 = Pessoa('Juliano', 37)
+
+pessoa_1.comer('Maçã')
+pessoa_1.parar_comer()
+pessoa_1.falar('Carro')
